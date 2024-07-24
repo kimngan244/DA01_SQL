d8bde62cfb2243373cbefc5966e36163b50d3d5b
kimngan244 <pealittle894042@gmail.com>
2024-07-24 22:50:38 +0700

    Update Day23_baitap.sql

--- Day23_baitap.sql
+++ Day23_baitap.sql
@@ -40,3 +40,34 @@ from cte)
 select YEAR_ID, PRODUCTLINE, revenue, rank
 from cte2
 where rank=1
+
+5)
+with cte as
+(select customername,
+current_date-max(orderdate) as R,
+count(distinct ordernumber) as F,
+sum(sales) as M
+from public.sales_dataset_rfm_prj_clean
+group by customername)
+	
+, cte2 as (select customername, 
+ntile(5) over(order by R desc) as R_score,
+ntile(5) over(order by F desc) as F_score,
+ntile(5) over(order by M desc) as M_score
+from cte)
+
+,cte3 as 
+(select customername,
+cast(R_score as varchar)||cast(F_score as varchar)||cast(M_score as varchar) as RFM_score
+from cte2)
+
+,cte4 as 
+(select a.customername, b.segment, a.RFM_score
+from cte3 a
+join segment_score b
+on a.RFM_score=b.scores)
+
+select segment, count(distinct customername) as count
+from cte4
+group by segment
+order by count(distinct customername) desc
